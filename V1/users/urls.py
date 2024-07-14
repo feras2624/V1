c02f07695e28ff7d8ba3873adbd448ad9ecdcdf6
feras2624 <rxm@DESKTOP-H10O83V>
2024-07-14 15:44:01 +0300

    add home page

--- V1/users/urls.py
+++ V1/users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path 
 from . import views
 urlpatterns = [
-    path('user/login',views.login),
+    path('user/login',views.login_view,name='login_view'),
+    path('user/home',views.home_view,name='home_view'),
+    path('user/logout',views.logout_view,name='logout_view'),
 ]
--- V1/users/views.py
+++ V1/users/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib import auth,messages
-def login(request):
+from django.contrib.auth import login , logout
+from django.contrib.auth.decorators import login_required
+def login_view(request):
     if request.method == "POST":
         email=request.POST.get("email")
         password=request.POST.get("password")
         user=auth.authenticate(email=email,password=password)
         print(email,"   ",password,"    ",user)
         if user is not None:
-            messages.success(request,"Wellcome")
+            login(request,user)
+            return redirect('home_view')
         else:
             messages.error(request,"Invalid Email or Password",extra_tags="danger")
         return render(request,"users/index.html")
     else:
         return render(request,"users/index.html")
+
+@login_required(login_url='login_view')
+def home_view(request):
+    return render(request,"users/home.html")
+
+@login_required(login_url='login_view')
+def logout_view(request):
+    logout(request)
+    messages.error(request,"You Were Logged Out",extra_tags="danger")
+    return redirect("login_view")
